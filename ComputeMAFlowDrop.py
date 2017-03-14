4f184213d4a446584216ac75c4eb554ece307580
Miguel O. Bernabeu <miguel.bernabeu@ed.ac.uk>
2017-03-14 10:14:13 +0000

    Refactor definition of Path objects out of loop

--- ComputeMAFlowDrop.py
+++ ComputeMAFlowDrop.py
@@ -25,6 +25,29 @@ if __name__ == '__main__':
     maBodyRoiFile = args.ma_body_roi_file
     subregionRoiFile = args.subregion_of_interest_file
     pixelPerMetre = 1e6 * args.pixels_per_micron
+
+    # Read MA body delineation from .csv file
+    wholeBodyCoords = np.genfromtxt (maBodyRoiFile, delimiter=",")
+    # Create a closed polygon from the delineation by repeating the first vertex at the end
+    wholeBodyCoords = np.append(wholeBodyCoords, [wholeBodyCoords[0]], axis=0)
+    # Swap axes for consistent XY convention
+    wholeBodyCoords[:, [0, 1]] = wholeBodyCoords[:, [1, 0]]
+    # Turn pixel number into spatial coordinates
+    wholeBodyCoords /= pixelPerMetre
+    # Path object allows inside-the-polygon lookups
+    wholeBodyPolygon = Path(wholeBodyCoords)
+
+    if subregionRoiFile != None:
+        # Read subregion delineation from .csv file
+        subregionCoords = np.genfromtxt (subregionRoiFile, delimiter=",")
+        # Create a closed polygon from the delineation by repeating the first vertex at the end
+        subregionCoords = np.append(subregionCoords, [subregionCoords[0]], axis=0)
+        # Swap axes for consistent XY convention
+        subregionCoords[:, [0, 1]] = subregionCoords[:, [1, 0]]
+        # Turn pixel number into spatial coordinates
+        subregionCoords /= pixelPerMetre
+        # Path object allows inside-the-polygon lookups
+        subregionPolygon = Path(subregionCoords)
     
     for (variableName, fileName) in flowVariables.iteritems():
         resultsFileName = os.path.join(resultsFolder, 'Extracted', fileName)
@@ -32,18 +55,6 @@ if __name__ == '__main__':
         resultsProperties = ExtractedProperty(resultsFileName)
         lastTimestep = resultsProperties.times[-1]
         resultsLastTimeStep = resultsProperties.GetByTimeStep(lastTimestep)
-
-        # Read MA body delineation from .csv file
-        wholeBodyCoords = np.genfromtxt (maBodyRoiFile, delimiter=",")
-        # Create a closed polygon from the delineation by repeating the first vertex at the end
-        wholeBodyCoords = np.append(wholeBodyCoords, [wholeBodyCoords[0]], axis=0)
-        # Swap axes for consistent XY convention
-        wholeBodyCoords[:, [0, 1]] = wholeBodyCoords[:, [1, 0]]
-        # Turn pixel number into spatial coordinates
-        wholeBodyCoords /= pixelPerMetre
-
-        # Path object allows inside-the-polygon lookups
-        wholeBodyPolygon = Path(wholeBodyCoords)
 
         # Flatten flow domain in 2D
         flowDomainCoords = resultsLastTimeStep.position[:,:2]
@@ -60,21 +71,10 @@ if __name__ == '__main__':
         ma_mask = wholeBodyPolygon.contains_points(flowDomainCoords)
         assert np.any(ma_mask), 'No results found within the ROI. Check intermediate .png files for overlap with the flow domain'
         
+        # If no subregion of interest was provided we compute drops on whole MA
         if subregionRoiFile == None:
             roi_mask = ma_mask
         else:
-            # Read subregion delineation from .csv file
-            subregionCoords = np.genfromtxt (subregionRoiFile, delimiter=",")
-            # Create a closed polygon from the delineation by repeating the first vertex at the end
-            subregionCoords = np.append(subregionCoords, [subregionCoords[0]], axis=0)
-            # Swap axes for consistent XY convention
-            subregionCoords[:, [0, 1]] = subregionCoords[:, [1, 0]]
-            # Turn pixel number into spatial coordinates
-            subregionCoords /= pixelPerMetre
-
-            # Path object allows inside-the-polygon lookups
-            subregionPolygon = Path(subregionCoords)
-
             # Visual check for geometrical consistency
             fig = plt.figure()
             ax = fig.add_subplot(111)
@@ -87,16 +87,15 @@ if __name__ == '__main__':
             roi_mask = subregionPolygon.contains_points(flowDomainCoords)
             assert np.any(roi_mask), 'No results found within the ROI. Check intermediate .png files for overlap with the flow domain'
 
-
         results = getattr(resultsLastTimeStep, variableName)
         if variableName in ['tangentialprojectiontraction', 'velocity']:
             results = np.linalg.norm(results, axis=1)
 
         # Extract the subset of results contained inside/outside the ROI
         mean_roi = np.mean(results[roi_mask])
-        mean_non_roi = np.mean(results[np.logical_not(ma_mask)])
-        print "{} mean drop ratio: {}".format(variableName, mean_non_roi / mean_roi)
+        mean_non_ma = np.mean(results[np.logical_not(ma_mask)])
+        print "{} mean drop ratio: {}".format(variableName, mean_non_ma / mean_roi)
 
         std_roi = np.std(results[roi_mask])
-        std_non_roi = np.std(results[np.logical_not(ma_mask)])
-        print "{} std drop ratio: {}".format(variableName, std_non_roi / std_roi)
+        std_non_ma = np.std(results[np.logical_not(ma_mask)])
+        print "{} std drop ratio: {}".format(variableName, std_non_ma / std_roi)
