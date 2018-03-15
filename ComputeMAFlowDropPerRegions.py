c3754c2b569b8490c816f318d4d7e70a805a4f63
Miguel O. Bernabeu <miguel.bernabeu@ed.ac.uk>
2018-03-15 13:01:50 +0000

    Reimplement CentrelineSideChecker based on splitting MA mask in two polygons along the centreline and check for point inside polygon

--- ComputeMAFlowDropPerRegions.py
+++ ComputeMAFlowDropPerRegions.py
@@ -10,7 +10,7 @@ import matplotlib.patches as patches
 import pandas as pd
 import vtk
 from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
-from shapely.geometry import LineString, MultiLineString
+from shapely.geometry import MultiLineString, Polygon, Point
 
 flowVariables = {'tangentialprojectiontraction' : 'surface-tractions.xtr',
                  'shearrate' : 'wholegeometry-shearrate.xtr'}
@@ -111,24 +111,33 @@ if __name__ == '__main__':
         centreline = transform_filter.GetOutput()
 
         class CentrelineSideChecker():
-            def __init__(self, centreline):
+            def __init__(self, centreline, whole_body_coords):
                 self.centreline = centreline
-                self.crossing_checker = self._centreline_to_MultiLineString(centreline)
-                self.xy_inside_bend = np.mean([centreline.GetPoints().GetPoint(centreline_point_id) for centreline_point_id in range(centreline.GetNumberOfPoints())], axis=0)[:2]
 
-            def _centreline_to_MultiLineString(self, centreline):
+                # Create a polygon defining the MA body and a second one with a thin dilation of the MA centreline
+                ma_polygon = Polygon(whole_body_coords)
+                dilated_centreline = self._centrelineToThinPolygon()
+
+                # Intersect previous two polygons to split MA body in two parts
+                ma_hemespheres = ma_polygon.difference(dilated_centreline)
+                assert len(ma_hemespheres) == 2, 'Centreline must split MA body in two parts'
+
+                smaller_half_id = np.argmin([half.area for half in ma_hemespheres])
+                self.smaller_ma_half = ma_hemespheres[smaller_half_id]
+
+            def _centrelineToThinPolygon(self, width=0.25e-6):
                 segment_collection = []
-                for cell_id in range(centreline.GetNumberOfCells()):
-                    segment_collection.append([centreline.GetCell(cell_id).GetPoints().GetPoint(0)[:2], 
-                                               centreline.GetCell(cell_id).GetPoints().GetPoint(1)[:2]])
-                return MultiLineString(segment_collection)
+                for cell_id in range(self.centreline.GetNumberOfCells()):
+                    segment_collection.append([np.array(self.centreline.GetCell(cell_id).GetPoints().GetPoint(0)),
+                                               np.array(self.centreline.GetCell(cell_id).GetPoints().GetPoint(1))])
 
-            def is_point_in_concave_side(self, coord):
-                xy_coord = coord[:2]
-                coord_to_inside_point = LineString([xy_coord, self.xy_inside_bend])
-                return not self.crossing_checker.crosses(coord_to_inside_point)
+                centreline = MultiLineString(segment_collection)
+                return centreline.buffer(width)
 
-        centreline_side_checker = CentrelineSideChecker(centreline)
+            def is_point_in_smaller_side(self, coord):
+                return self.smaller_ma_half.contains(Point(coord[0], coord[1]))
+
+        centreline_side_checker = CentrelineSideChecker(centreline, wholeBodyCoords)
  
         # Centreline radii vector in metres
         radii = 1e-6 * vtk_to_numpy(centreline.GetPointData().GetArray('Radius'))
@@ -155,10 +164,10 @@ if __name__ == '__main__':
             if unit_distance > 1:
                 region_id -= 1
 
-            inside_bend = centreline_side_checker.is_point_in_concave_side(coord)
+            in_smaller_half = centreline_side_checker.is_point_in_smaller_side(coord)
             region_mapping = {True : range(number_subregions_each_side_centreline-1, -1, -1),
                               False : range(number_subregions_each_side_centreline, 2*number_subregions_each_side_centreline)}
-            region_id = region_mapping[inside_bend][region_id]
+            region_id = region_mapping[in_smaller_half][region_id]
 
             vtk_points_within_roi.InsertNextPoint(coord);
             classification[coord_id] = region_id
